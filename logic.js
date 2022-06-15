6819581fec16aa055f5f001177eb3ca58eee0e19
darshan-k3 <darshan.kumaresan@gmail.com>
2022-06-15 10:18:33 +0100

    Changed structure of arrays

--- logic.js
+++ logic.js
@@ -2,7 +2,11 @@
   Arrays to store the messages and the descriptions
   */
   const conversation = ['Hiya how\'s it going!', 'I\'m good thanks', 'What are you doing tonight?',  'Not much', 'Ok fair enough',  ]
-  const users = {'John' : 'Name: John<br></br> Age: 34 <br></br> Works as a chef<br></br> Is great fun.', 'Jane' :'Name: Jane<br></br> Age: 25 <br></br> Works as a chef<br></br> Likes gardening.', 'Bob': 'Name: Bob<br></br> Age: 80<br></br> Retired - likes playing chess in his spare time<br></br> Likes playing the piano', 'Mary':'Name: Mary<br></br> Age: 34<br></br> Works as a chef<br></br> Is great fun.', 'Steve': 'Name: Steve<br></br> Age: 34<br></br> Works as a chef<br></br> Is great fun.'}
+  const people = ['John', 'Jane', 'Bob', 'Mary', 'Steve'];
+  const ages = [34, 25, 80, 36, 19];
+  const jobs = ['Chef', 'Retired army veteran', 'Waiter', 'Builder', 'Cartographer'];
+  const hobbies = ['Hiking', 'Reading', 'Playing Chess', 'Playing the Piano', 'Gardening'];
+  const hobbiesDescriptions = ['I like to hike in the woods', 'I like to read', 'I like to play chess', 'I like to play the piano', 'I like to garden'];
   /*
   Counters used to iterate through the arrays
   */
@@ -15,10 +19,20 @@
     return conversation[countConversation];  
   }
   function getUser() {
-    return Object.keys(users)[countUsers]
+    return people[countUsers]
   }
-  function getDescription() {
-    return users[getUser()]
+  function getAge(){
+    return ages[countUsers];
+  }
+  function getJob(){
+    return jobs[countUsers];
+  }
+  function getHobbies(){
+    return hobbiesDescriptions[countUsers];
+  }
+  function generateDescription(){
+    let messageString = getUser() + "<br></br>" + getAge() + " years old<br></br>" + "Is currently a " + getJob() + "<br></br>" + getHobbies();
+    return messageString;
   }
 
   //Interval used to build a new message every 5 seconds
@@ -31,7 +45,7 @@
     element.style.textAlign = 'right';
     element.appendChild(tag);
     var description = document.getElementById("description");
-    description.innerHTML = getDescription();
+    description.innerHTML = generateDescription();
     checkCount()
   }, 5000);
   //Function to check if the counters have reached the end of the arrays
@@ -41,7 +55,7 @@
     if(countConversation > conversation.length - 1){
       countConversation = 0;
     }
-    if(countUsers > Object.keys(users).length - 1){
+    if(countUsers > people.length - 1){
       countUsers = 0;
     }
   }
